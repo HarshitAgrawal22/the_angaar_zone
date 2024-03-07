a7f1c7f2cb805a1a2c31f576f0bd19952853350f
HarshitAgrawal <115237538+HarshitAgrawal22@users.noreply.github.com>
2024-03-08 03:07:22 +0530

    models updated

--- angaar_zone/accounts/models.py
+++ angaar_zone/accounts/models.py
@@ -1,3 +1,78 @@
+from typing import Any
 from django.db import models
+from django.contrib.auth.models import User
+
+from imagekit.processors import ResizeToFit
+from imagekit.models import ProcessedImageField
+
+from home.models import Course
+
 
 # Create your models here.
+
+class Students(User):
+    phone_number=models.CharField(max_length=10)
+    address =models.TextField()
+    parent_phone_number=models.CharField(max_length=10,null=True,blanck=True)
+    gender=models.CharField(choices=(
+        ("female","female"),("male","male"),("Prefer not to say","Preer not to say")
+    ))
+    course=models.ManyToManyField(Course ,blank=False,related_name="students",
+    on_delete=models.CASCADE)
+    
+    bio = models.TextField()
+    isStudent=models.BooleanField(blank=False,null=False)
+    
+    profile_pic=ProcessedImageField(upload_to="students_profile",processors=[ResizeToFit(width=500,height=600)],format="JPEG",
+                                    options={"quality":90})
+    
+    # models.ImageField(upload_to="student_profile",blank=True,null=True,default="student_profile/catpic.jpg")
+    
+    def __str__(self):
+        return self.first_name
+    
+    
+    def call(self)->dict:
+        return {
+            "username":self.username,
+            "first_name":self.first_name,
+            "last_name":self.last_name,
+        }
+        
+    class Meta:
+        verbose_name_plural = 'Students'
+        
+        
+        verbose_name = 'Student'
+        
+
+
+class Teacher(User):
+    phone_number=models.CharField(blank=False,null=False)
+    bio=models.TextField(null=True,blank=True)
+    
+    gender=models.CharField(choices=(("Female","Female"),("Male","Male"),("Prefer not to tell","Prefer not to tell")))
+    
+    profile_pic=ProcessedImageField(upload_to="teacher_profile",
+                                    processors=[ResizeToFit(width=500,height=600)],
+                                    format="JPEG",options={"quality":90}
+                                    )
+    course=models.ForeignKey(Course,related_name="teachers",blank=False,null=False)
+    
+    def __str__(self):
+        return self.first_name+self.last_name
+    
+    
+    def __call__(self)-> dict:
+        return {
+            "username":self.username,
+            "first_name":self.first_name,
+            "last_name":self.last_name
+            
+        }
+        
+    class Meta:
+        verbose_name_plural = 'Teachers'
+        
+        verbose_name = 'Teacher'
+        
--- angaar_zone/home/models.py
+++ angaar_zone/home/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from accounts.models import *
 
+from django.contrib.auth.models import User 
 # Create your models here.
+
+
+class Course(models.Model):
+    name=models.CharField(blank=False,null=False,max_length=30)
+    description=models.TextField(blank=False,null=False)
+    
+    
+    
+    
+    
+class Session(models.Model):
+    course=models.ForeignKey(Course,null=True,blank=True)
+    teacher=models.ForeignKey(Teacher,related_name="sessions",blank=False,null=False)
+    title=models.CharField(null=False,blank=False,max_length=40)
+    
+    time=models.DateTimeField(auto_add_now=True)
+    link=models.URLField()
+    
+    is_Complete=models.BooleanField(null=False,default=False)
+    
+    notes=models.FileField(null=True,blank=True,upload_to="notes_and_files")
+    
+    
+class Chat(models.Model):
+    text=models.TextField()
+    
+    sender=models.ForeignKey(User,blank=False,null=False)
+    receiver=models.ForeignKey(User,null=False,blank=False)
+    time=models=models.DateTimeField(auto_add_now=True)
+    
+    
