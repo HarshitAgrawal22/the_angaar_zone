ec8fd181382665536bac020c3f6fa4ab3a8e09c0
HarshitAgrawal <haharshitagrawal22@gmail.com>
2024-03-13 23:38:09 +0530

    trying something new here

--- angaar_zone/flutter/serializers.py
+++ angaar_zone/flutter/serializers.py
@@ -14,4 +14,20 @@ class StudentRegistrationSerializers(serializers.ModelSerializer):
                 "write_only":True
             }
         }
-        
+    
+    def validate(self, attrs):
+        #TODO add the conditions needed to validate the student 
+        password=attrs.get("password")
+        username=attrs.get("username")
+        if password is not None and username is not None:
+            return attrs
+        raise serializers.ValidationError('Ivalid data sent')
+    
+    
+    
+    def create(self,validated_data):
+        student=Students.objects.create(username=validated_data.get("username"))
+        # TODO we need to add email field while registration
+        student.set_password(validated_data.get("password"))
+        student.save()
+        return student
